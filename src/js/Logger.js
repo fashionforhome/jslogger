1e88f0ec2aa26ff050aca181b2f8fe0151bcc829
tino_stoeckel <tino.stoeckel@fashionforhome.de>
2014-06-11 17:55:31 +0200

    whitespace-development

--- src/js/Logger.js
+++ src/js/Logger.js
@@ -1,34 +1,34 @@
 var Logger = function() {
 
-    this.EMERGENCY = 1;
-    this.ALERT     = 2;
-    this.CRITICAL  = 3;
-    this.ERROR     = 4;
-    this.WARNING   = 5;
-    this.NOTICE    = 6;
-    this.INFO      = 7;
-    this.DEBUG     = 8;
+	this.EMERGENCY  = 1;
+	this.ALERT      = 2;
+	this.CRITICAL   = 3;
+	this.ERROR      = 4;
+	this.WARNING    = 5;
+	this.NOTICE     = 6;
+	this.INFO       = 7;
+	this.DEBUG      = 8;
 
-    var rendererList = [];
+	var rendererList = [];
 
-    this.addRenderer = function(renderers) {
-        rendererList.push(renderers);
-        return this;
-    };
+	this.addRenderer = function(renderers) {
+		rendererList.push(renderers);
+		return this;
+	};
 
-//    this.setLogLevel = function(level) {
-//        if(level !== parseInt(level)){
-//            console.log('WTF are you giving me!');
-//        } else {
-//            console.log("correct");
-//        }
-//    };
+//	this.setLogLevel = function(level) {
+//		if(level !== parseInt(level)){
+//			console.log('WTF are you giving me!');
+//		} else {
+//			console.log("correct");
+//		}
+//	};
 
-     this.log = function(msg,exception , logLevel) {
-         var length = rendererList.length;
-         for(var i=0; i<length; i++){
-             rendererList[i].render(msg, exception, logLevel );
-         }
+	this.log = function(msg,exception , logLevel) {
+		var length = rendererList.length;
+		for(var i=0; i<length; i++){
+			rendererList[i].render(msg, exception, logLevel );
+		}
 
-    };
+	};
 }
