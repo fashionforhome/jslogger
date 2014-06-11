9f132e6bd1f85ae9c0a73430321900b1e7c5f4f1
rainer.maucher <rainer.maucher@fashionforhome.de>
2014-06-11 17:18:01 +0200

    Initial Commit, for application structure and basic composer structure

---
+++ lib/Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * Just a example class to demonstrate Namespaceing
+ */
+
+namespace F4H\Logger;
+
+class Test
+{
+	public function __construct()
+	{
+		die('huba');
+	}
+}
---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+// namespace example
+use F4H\Logger\Test;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+new Test();
