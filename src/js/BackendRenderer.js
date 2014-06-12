3a38dbef0277d353a6c688c1d59dfe5fa33bc86b
tino_stoeckel <tino.stoeckel@fashionforhome.de>
2014-06-12 11:50:47 +0200

    - setup the backendRenderer

--- src/js/BackendRenderer.js
+++ src/js/BackendRenderer.js
@@ -1,6 +1,20 @@
-var BackendRenderer = function() {
+var BackendRenderer = function(config) {
 
-	this.render = function(){
-		alert('Backend');
-	}
-}
+	/*@var _config - expected as JSON: {'url':"http://www.abc.com", 'consoleResponse':false} */
+	var _config = config;
+
+	this.render = function(msg, logType, optionalData){
+		$.post(
+			_config['url'],
+			{ 'logType': logType, 'message': _buildMessage(msg, optionalData) }
+		).done( function( data ) {
+			if (_config['consoleResponse'] === true) {
+				window.console && console.log('BackendRenderer here:', data);
+			}
+		});
+	};
+
+	var _buildMessage = function(msg, optionalData){
+		return msg;
+	};
+};
