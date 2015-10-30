e87903cea44bf12ad8af66634fbca8e096728d4b
Rebel-L <dj@rebel-l.net>
2015-10-30 01:34:06 +0100

    added license header to lib

--- lib/Log.php
+++ lib/Log.php
@@ -1,5 +1,21 @@
 <?php
-
+/**
+ * This is the ..... [Description]
+ *
+ * @category Lib
+ * @package Lib
+ *
+ * @author Rainer Maucher <rainer.maucher@fashionforhome.de>
+ *
+ * @copyright (c) 2014 by fashion4home GmbH <www.fashionforhome.de>
+ * @license GPL-3.0
+ * @license http://opensource.org/licenses/GPL-3.0 GNU GENERAL PUBLIC LICENSE
+ *
+ * @version 1.0.0
+ *
+ * Date: 13.06.2014
+ * Time: 22:04
+ */
 namespace F4H\Logger;
 
 use Monolog\Logger;
--- lib/Test.php
+++ lib/Test.php
@@ -1,8 +1,20 @@
 <?php
 /**
- * Created by PhpStorm.
+ * This is the ..... [Description]
  *
- * Just a example class to demonstrate Namespaceing
+ * @category Lib
+ * @package Lib
+ *
+ * @author Rainer Maucher <rainer.maucher@fashionforhome.de>
+ *
+ * @copyright (c) 2014 by fashion4home GmbH <www.fashionforhome.de>
+ * @license GPL-3.0
+ * @license http://opensource.org/licenses/GPL-3.0 GNU GENERAL PUBLIC LICENSE
+ *
+ * @version 1.0.0
+ *
+ * Date: 11.06.2014
+ * Time: 22:04
  */
 
 namespace F4H\Logger;
